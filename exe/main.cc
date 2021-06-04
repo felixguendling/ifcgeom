5eaded606c93840ab5bfa5f827c3da68be7549a2
Felix Gündling <felix.guendling@gmail.com>
2021-06-05 01:28:48 +0200

    wip

--- exe/main.cc
+++ exe/main.cc
@@ -3,8 +3,31 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+#include "cista/mmap.h"
+
+#include "IFC2X3/IfcRepresentationItem.h"
+#include "IFC2X3/IfcShapeRepresentation.h"
+#include "IFC2X3/parser.h"
+
 class MyApp : public ci::app::App {
   void setup() override {
+    auto const ifc = cista::mmap{"/home/felix/Downloads/Wand_ArchiCad_BRep.ifc"
+                                 //"../AC20-Institute-Var-2.ifc"
+                                 ,
+                                 cista::mmap::protection::READ};
+    auto const model = IFC2X3::parse({ifc.data(), ifc.size()});
+    for (auto const& e : model.entity_mem_) {
+      auto const sr =
+          dynamic_cast<IFC2X3::IfcShapeRepresentation const*>(e.get());
+      if (sr == nullptr) {
+        continue;
+      }
+
+      for (auto const& item : sr->Items_) {
+        std::cout << item->name() << "\n";
+      }
+    }
+
     auto const color = ci::gl::getStockShader(ci::gl::ShaderDef().color());
     cam_.lookAt(glm::vec3{2.0F, 3.0F, 1.0F}, glm::vec3{0});
     cam_.setPerspective(40.0F, getWindowAspectRatio(), 0.01f, 100.0F);
@@ -16,7 +39,7 @@ class MyApp : public ci::app::App {
   }
 
   void draw() override {
-    ci::gl::clear(ci::Color::gray(0.5f));
+    ci::gl::clear(ci::Color::white());
     ci::gl::ScopedMatrices push;
     ci::gl::setMatrices(cam_);
 
@@ -28,9 +51,6 @@ class MyApp : public ci::app::App {
     {
       ci::gl::ScopedColor clr(ci::Color(1, 0.5f, 0));
       ci::gl::ScopedModelMatrix model;
-
-      //      ci::gl::multModelMatrix(glm::translate(bounds.getCenter()) *
-      //                              glm::scale(bounds.getSize()));
       cube_->draw();
     }
   }
