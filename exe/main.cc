7c1f91cb84f21599d2d98b1b99ffa1040d448c35
Felix Gündling <felix.guendling@gmail.com>
2021-06-20 11:18:09 +0200

    wip

--- exe/main.cc
+++ exe/main.cc
@@ -1,64 +1,149 @@
-#include "cinder/CameraUi.h"
-#include "cinder/app/App.h"
-#include "cinder/app/RendererGl.h"
-#include "cinder/gl/gl.h"
+/*
+    This file is part of Magnum.
 
-#include "cista/mmap.h"
+    Original authors — credit is appreciated but not required:
 
-#include "IFC2X3/IfcRepresentationItem.h"
-#include "IFC2X3/IfcShapeRepresentation.h"
-#include "IFC2X3/parser.h"
+        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021
+             — Vladimír Vondruš <mosra@centrum.cz>
 
-class MyApp : public ci::app::App {
-  void setup() override {
-    auto const ifc = cista::mmap{"/home/felix/Downloads/Wand_ArchiCad_BRep.ifc"
-                                 //"../AC20-Institute-Var-2.ifc"
-                                 ,
-                                 cista::mmap::protection::READ};
-    auto const model = IFC2X3::parse({ifc.data(), ifc.size()});
-    for (auto const& e : model.entity_mem_) {
-      auto const sr =
-          dynamic_cast<IFC2X3::IfcShapeRepresentation const*>(e.get());
-      if (sr == nullptr) {
-        continue;
-      }
+    This is free and unencumbered software released into the public domain.
 
-      for (auto const& item : sr->Items_) {
-        std::cout << item->name() << "\n";
-      }
-    }
+    Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+    this software, either in source code form or as a compiled binary, for any
+    purpose, commercial or non-commercial, and by any means.
 
-    auto const color = ci::gl::getStockShader(ci::gl::ShaderDef().color());
-    cam_.lookAt(glm::vec3{2.0F, 3.0F, 1.0F}, glm::vec3{0});
-    cam_.setPerspective(40.0F, getWindowAspectRatio(), 0.01f, 100.0F);
-    cam_ui_ = ci::CameraUi(&cam_, getWindow());
-    cube_ = ci::gl::Batch::create(ci::geom::WireCube{}, color);
-    wire_plane_ = ci::gl::Batch::create(
-        ci::geom::WirePlane().size(glm::vec2(10)).subdivisions(glm::ivec2(10)),
-        color);
-  }
+    In jurisdictions that recognize copyright laws, the author or authors of
+    this software dedicate any and all copyright interest in the software to
+    the public domain. We make this dedication for the benefit of the public
+    at large and to the detriment of our heirs and successors. We intend this
+    dedication to be an overt act of relinquishment in perpetuity of all
+    present and future rights to this software under copyright law.
 
-  void draw() override {
-    ci::gl::clear(ci::Color::white());
-    ci::gl::ScopedMatrices push;
-    ci::gl::setMatrices(cam_);
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
-    {
-      ci::gl::ScopedColor color(ci::Color::gray(0.2f));
-      wire_plane_->draw();
-    }
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/GL/Renderer.h>
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Matrix4.h>
+#include <Magnum/MeshTools/CompressIndices.h>
+#include <Magnum/MeshTools/Interleave.h>
+#include <Magnum/Platform/Sdl2Application.h>
+#include <Magnum/Primitives/Cube.h>
+#include <Magnum/SceneGraph/Camera.h>
+#include <Magnum/SceneGraph/Drawable.h>
+#include <Magnum/SceneGraph/MatrixTransformation3D.h>
+#include <Magnum/SceneGraph/Scene.h>
+#include <Magnum/Shaders/PhongGL.h>
+#include <Magnum/Trade/MeshData.h>
 
-    {
-      ci::gl::ScopedColor clr(ci::Color(1, 0.5f, 0));
-      ci::gl::ScopedModelMatrix model;
-      cube_->draw();
-    }
-  }
+namespace Magnum {
+namespace Examples {
 
-  ci::CameraPersp cam_;
-  ci::CameraUi cam_ui_;
-  ci::gl::BatchRef cube_;
-  ci::gl::BatchRef wire_plane_;
+using namespace Magnum::Math::Literals;
+
+typedef SceneGraph::Object<SceneGraph::MatrixTransformation3D> Object3D;
+typedef SceneGraph::Scene<SceneGraph::MatrixTransformation3D> Scene3D;
+
+class PrimitivesExample : public Platform::Application {
+public:
+  explicit PrimitivesExample(const Arguments& arguments);
+
+private:
+  void drawEvent() override;
+  void mousePressEvent(MouseEvent& event) override;
+  void mouseReleaseEvent(MouseEvent& event) override;
+  void mouseMoveEvent(MouseMoveEvent& event) override;
+
+  GL::Mesh _mesh;
+  Shaders::PhongGL _shader;
+
+  Scene3D _scene;
+  Matrix4 _transformation, _projection;
+  Color3 _color;
 };
 
-CINDER_APP(MyApp, ci::app::RendererGl(ci::app::RendererGl::Options().msaa(8)))
+PrimitivesExample::PrimitivesExample(const Arguments& arguments)
+    : Platform::Application{
+          arguments, Configuration{}.setTitle("Magnum Primitives Example")} {
+  GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+  GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+
+  Trade::MeshData cube = Primitives::cubeSolid();
+
+  GL::Buffer vertices;
+  vertices.setData(
+      MeshTools::interleave(cube.positions3DAsArray(), cube.normalsAsArray()));
+
+  std::pair<Containers::Array<char>, MeshIndexType> compressed =
+      MeshTools::compressIndices(cube.indicesAsArray());
+  GL::Buffer indices;
+  indices.setData(compressed.first);
+
+  _mesh.setPrimitive(cube.primitive())
+      .setCount(cube.indexCount())
+      .addVertexBuffer(std::move(vertices), 0, Shaders::PhongGL::Position{},
+                       Shaders::PhongGL::Normal{})
+      .setIndexBuffer(std::move(indices), 0, compressed.second);
+
+  _transformation =
+      Matrix4::rotationX(30.0_degf) * Matrix4::rotationY(40.0_degf);
+  _projection =
+      Matrix4::perspectiveProjection(
+          35.0_degf, Vector2{windowSize()}.aspectRatio(), 0.01f, 100.0f) *
+      Matrix4::translation(Vector3::zAxis(-10.0f));
+  _color = Color3::fromHsv({35.0_degf, 1.0f, 1.0f});
+}
+
+void PrimitivesExample::drawEvent() {
+  GL::defaultFramebuffer.clear(GL::FramebufferClear::Color |
+                               GL::FramebufferClear::Depth);
+
+  _shader.setLightPositions({{1.4f, 1.0f, 0.75f, 0.0f}})
+      .setDiffuseColor(_color)
+      .setAmbientColor(Color3::fromHsv({_color.hue(), 1.0f, 0.3f}))
+      .setTransformationMatrix(_transformation)
+      .setNormalMatrix(_transformation.normalMatrix())
+      .setProjectionMatrix(_projection)
+      .draw(_mesh);
+
+  swapBuffers();
+}
+
+void PrimitivesExample::mousePressEvent(MouseEvent& event) {
+  if (event.button() != MouseEvent::Button::Left) return;
+
+  event.setAccepted();
+}
+
+void PrimitivesExample::mouseReleaseEvent(MouseEvent& event) {
+  _color = Color3::fromHsv({_color.hue() + 50.0_degf, 1.0f, 1.0f});
+
+  event.setAccepted();
+  redraw();
+}
+
+void PrimitivesExample::mouseMoveEvent(MouseMoveEvent& event) {
+  if (!(event.buttons() & MouseMoveEvent::Button::Left)) return;
+
+  Vector2 delta =
+      3.0f * Vector2{event.relativePosition()} / Vector2{windowSize()};
+
+  _transformation = Matrix4::rotationX(Rad{delta.y()}) * _transformation *
+                    Matrix4::rotationY(Rad{delta.x()});
+
+  event.setAccepted();
+  redraw();
+}
+
+}  // namespace Examples
+}  // namespace Magnum
+
+MAGNUM_APPLICATION_MAIN(Magnum::Examples::PrimitivesExample)
